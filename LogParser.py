92897531b9dc96222bc8f0fddbdc0429b6e80fd1
Paul Kobialka <p.kobialka@web.de>
2024-11-26 17:47:33 +0100

    assert that model exists

--- LogParser.py
+++ LogParser.py
@@ -203,6 +203,7 @@ class SpotifyParser(LogParser):
                 f.write('\n')
         model = run_JAlergia(path_to_data_file=storage_file, automaton_type='mdp', eps=0.9,
                      path_to_jAlergia_jar='../jAlergia/alergia.jar')
+        assert model, f'None model for file {storage_file}'
         os.remove(storage_file)
         # model = run_JAlergia(path_to_data_file=random.sample(self.data_environment, min(len(self.data_environment), self.number_samples)), automaton_type='mdp', eps=0.9,
         #              path_to_jAlergia_jar='../jAlergia/alergia.jar')
