c2117bcd59e53d9a5f7157c4f9fc9ca498d6efa8
Paul Kobialka <p.kobialka@web.de>
2024-11-27 15:12:08 +0100

    updated Result.py for status codes

--- Result.py
+++ Result.py
@@ -4,14 +4,20 @@ class Result:
     time = -1
     value = -1
     target_prob = -1
+    timeout = -1
+    gap = -1
     strategy = {}
+    status = -1 # https://docs.gurobi.com/projects/optimizer/en/current/reference/numericcodes/statuscodes.html
     
-    def __init__(self, time, value, target_prob, strategy):
+    def __init__(self, time, value, target_prob, strategy, timeout, gap, status = -1):
         self.time = time
         self.value = value
         self.target_prob = target_prob
         self.strategy = strategy
+        self.timeout = timeout
+        self.gap = gap
+        self.status = status
         
     def df(self):
-        d = {'time' : self.time, 'value' : self.value, 'target_prob' : self.target_prob}
+        d = {'time' : self.time, 'value' : self.value, 'target_prob' : self.target_prob, 'timeout' : self.timeout, 'gap' : self.gap, 'status' : self.status}
         return pd.DataFrame([d])
