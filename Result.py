83ef9e3a80572b0ea0fa68a8052bc68906cfdc72
Paul Kobialka <p.kobialka@web.de>
2025-01-09 14:10:01 +0100

    removed shared attributes

--- Result.py
+++ Result.py
@@ -1,14 +1,6 @@
 import pandas as pd
 
 class Result:
-    time = -1
-    value = -1
-    target_prob = -1
-    timeout = -1
-    gap = -1
-    strategy = {}
-    status = -1 # https://docs.gurobi.com/projects/optimizer/en/current/reference/numericcodes/statuscodes.html
-    
     def __init__(self, time, value, target_prob, strategy, timeout, gap, status = -1):
         self.time = time
         self.value = value
@@ -16,7 +8,7 @@ class Result:
         self.strategy = strategy
         self.timeout = timeout
         self.gap = gap
-        self.status = status
+        self.status = status # https://docs.gurobi.com/projects/optimizer/en/current/reference/numericcodes/statuscodes.html
         
     def df(self):
         d = {'time' : self.time, 'value' : self.value, 'target_prob' : self.target_prob, 'timeout' : self.timeout, 'gap' : self.gap, 'status' : self.status}
